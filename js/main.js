a6217a0d761a394bc8a71ece0f5a5dfb108da0f4
LeeroyDee <59417798+LeeroyDee@users.noreply.github.com>
2021-08-03 17:25:43 +0300

    create: referense

--- js/main.js
+++ js/main.js
@@ -5,3 +5,17 @@ document.getElementById("trigger").onclick = function () {
 function open() {
   document.getElementById("menu").classList.toggle("header__left--show");
 }
+
+
+const swiper = new Swiper('.reference-slider', {
+  slidesPerView: 1,
+  spaceBetween: 10,
+  direction: 'horizontal',
+  loop: false,
+
+  // Navigation arrows
+  navigation: {
+    nextEl: '.swiper-button-next',
+    prevEl: '.swiper-button-prev',
+  },
+});
