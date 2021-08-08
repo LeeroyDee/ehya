307dd1f831acdc09cdb7fb37ba19b73122ac5bcb
LeeroyDee <stas.dubikovckiy@mail.ru>
2021-08-08 18:05:50 +0300

    fix: up

--- js/main.js
+++ js/main.js
@@ -145,3 +145,17 @@ $("form").each(function () {
   });
 });
 $(".phone").mask("+7(999) 99-99-99");
+
+//правный скролл
+var t;
+function up() {
+  var top = Math.max(
+    document.body.scrollTop,
+    document.documentElement.scrollTop
+  );
+  if (top > 0) {
+    window.scrollBy(0, -100);
+    t = setTimeout("up()", 20);
+  } else clearTimeout(t);
+  return false;
+}
